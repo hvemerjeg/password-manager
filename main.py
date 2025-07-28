dd1e1cda6c00af7768583a9055068b5dc87fee6c
hvemerjeg <rafaelgulias@hotmail.com>
2025-07-28 10:20:13 -0400

    Change help message for --new-database

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ def main():
     parser.add_argument("--delete", help="""Delete a password. The password will be held in a different table for recovery.
                         Will be eliminated after 30 days""", action="store_true")
     parser.add_argument("--recover", help="Recover a deleted password. Passwords are held in a different table for decovery for 30 days", action="store_true")
-    parser.add_argument("--new-database", help="""Create a new database file to save your passwords.
-    Provide only the path to the directory without filename""", action="store_true")
+    parser.add_argument("--new-database", help="Create a new database file to save your passwords.", action="store_true")
     parser.add_argument("--database", help="Database file with passwords. File extension needs to end in pdbm")
     parser.add_argument("--passfile", help="""Passfile required (besides the password) to decrypt and encrypt database.
                         This file is the "something you have" factor. File extension needs to end in pdbmk""")
