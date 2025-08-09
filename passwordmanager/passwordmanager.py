4cfdca174cc1f0238be393941eb617f07e4edbcf
hvemerjeg <rafaelgulias@hotmail.com>
2025-08-09 07:13:38 -0400

    Continue developing

--- passwordmanager/passwordmanager.py
+++ passwordmanager/passwordmanager.py
@@ -8,9 +8,18 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 class PasswordManager:
-    def __init__(self, master_key:str, filename=None):
-        self.filename = filename
+    def __init__(self, master_key:str, database:str, passfile:str):
+        self.database = database
         self.master_key = master_key
+        self.passfile = passfile
+        self.conn = sqlite3.connect(self.database)
+        self.cursor = self.conn.cursor()
+
+    def createDefaultTable(self, random_string:str):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS
+        default(id INTEGER PRIMARY KEY, random_string text NOT NULL) 
+        """)
+
 
     def savePassword(self, service:str, username:str, password:str) -> None:
         pass
--- utils/initfiles.py
+++ utils/initfiles.py
@@ -1,19 +1,31 @@
 import logging
 import os
 import sys
-from utils import Singleton
+from passwordmanager import PasswordManager
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def initFiles(component:str):
-    if not os.path.exists(component):
-        logger.info("Creating %s", component)
-        try:
-            with open(component, "w") as f:
-                pass
-        except PermissionError as e:
-            sys.stderr.write(f"{e}\n")
-            exit(1)
-    else:
-        logger.info("%s already exists", component)
+class initFiles:
+    def __init__(self):
+        self.random_string = ""
+        while len(self.random_string) < 16:
+            for byte in os.urandom(32):
+                if byte in range(32, 127):
+                    self.random_string += chr(byte)
+
+    def initFiles(component:str, db=False):
+        if not os.path.exists(component):
+            logger.info("Creating %s", component)
+            try:
+                if db:
+                    with open(component, "w") as f:
+                        pass
+                else:
+                    with open(component, "w") as f:
+                        f.write(f"{self.random_string}\n")
+            except PermissionError as e:
+                sys.stderr.write(f"{e}\n")
+                exit(1)
+        else:
+            logger.info("%s already exists", component)
